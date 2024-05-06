d1973a5cdab6797150359be7c0e6ef72d0a6b0ed
Cosine-A <100404990+Cosine-A@users.noreply.github.com>
2024-05-06 14:15:06 +0900

    feat: 검색 버튼 눌렀을 때 키보드 숨김 처리 추가

--- app/src/main/java/kr/cosine/imagesearch/view/fragment/impl/SearchFragment.kt
+++ app/src/main/java/kr/cosine/imagesearch/view/fragment/impl/SearchFragment.kt
@@ -1,7 +1,13 @@
 package kr.cosine.imagesearch.view.fragment.impl
 
+import android.content.Context
+import android.content.Context.INPUT_METHOD_SERVICE
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
+import android.view.inputmethod.InputMethodManager
+import androidx.core.content.ContextCompat.getSystemService
 import androidx.lifecycle.flowWithLifecycle
 import androidx.lifecycle.lifecycleScope
 import kotlinx.coroutines.flow.collectLatest
@@ -9,26 +15,33 @@ import kotlinx.coroutines.launch
 import kr.cosine.imagesearch.databinding.FragmentSearchBinding
 import kr.cosine.imagesearch.view.fragment.BaseFragment
 
+
 class SearchFragment : BaseFragment<FragmentSearchBinding>(FragmentSearchBinding::inflate) {
 
     override val recyclerView get() = binding.searchRecyclerView
 
+    private val sharedPreferences by lazy { activity?.getSharedPreferences(FILE_NAME, Context.MODE_PRIVATE) }
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        hideKeyboard()
         registerSearchButton()
         registerViewModelEvent()
-    }
-
-    private fun hideKeyboard() {
-        binding.searchEditText.inputType = 0
+        loadInput()
     }
 
     private fun registerSearchButton() = with(binding) {
         searchButton.setOnClickListener {
             val input = searchEditText.text.toString()
+            if (input.isBlank()) return@setOnClickListener
+            hideKeyboard()
             searchViewModel.onSearch(input)
+            saveInput(input)
         }
+    }
+
+    private fun hideKeyboard() = activity?.let {
+        val manager = it.getSystemService(INPUT_METHOD_SERVICE) as InputMethodManager
+        manager.hideSoftInputFromWindow(it.currentFocus?.windowToken, InputMethodManager.HIDE_NOT_ALWAYS)
     }
 
     private fun registerViewModelEvent() {
@@ -37,5 +50,26 @@ class SearchFragment : BaseFragment<FragmentSearchBinding>(FragmentSearchBinding
                 searchAdapter.update(uiState.items)
             }
         }
+    }
+
+    private fun loadInput() {
+        sharedPreferences?.getString(INPUT_KEY, INPUT_DEFAULT_VALUE)?.let {
+            if (it.isBlank()) return
+            binding.searchEditText.setText(it)
+            searchViewModel.onSearch(it)
+        }
+    }
+
+    private fun saveInput(input: String) {
+        sharedPreferences?.edit()?.apply {
+            putString(INPUT_KEY, input)
+            apply()
+        }
+    }
+
+    private companion object {
+        const val FILE_NAME = "ImageSearch"
+        const val INPUT_KEY = "Input"
+        const val INPUT_DEFAULT_VALUE = ""
     }
 }
